8d8e961223feb3177c181ccef51b80d7820f2b0b
Hasala Kavinda <c.m.hasalakavinda@gmail.com>
2026-09-18 22:22:39 +0530

    Added the telescpe lsp configurations

--- init.lua
+++ init.lua
@@ -23,8 +23,3 @@ require("lazy").setup({
   },
   checker = { enabled = true },
 })
-
-
-	
-	
-	
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,37 @@
+return
+{{
+    "mason-org/mason.nvim",
+    opts = {}
+    },
+
+    {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+     ensure_installed = {
+	"lua_ls",
+     	"rust_analyzer",
+	"html",
+	"ts_ls",
+	"cssls",
+	"tailwindcss",
+	"pyright",
+	"eslint",
+     },
+     },
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+},
+{
+	"neovim/nvim-lspconfig",
+	config = function()
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+		vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+		vim.diagnostic.config({
+			virtual_text = true,
+		})
+	end
+}
+}
